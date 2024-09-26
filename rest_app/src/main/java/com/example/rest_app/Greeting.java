dae486f45489f119a51c7a157ca9c7749c5b1b6d
Tanmoy Mazumder <tanmoycoc44@gmail.com>
2024-09-26 09:24:26 +0530

    2nd

---
+++ rest_app/src/main/java/com/example/rest_app/Greeting.java
@@ -0,0 +1,3 @@
+package com.example.rest_app;
+
+public record Greeting(long id, String content) { }
---
+++ rest_app2/src/main/java/com/example/rest_app2/Greeting.java
@@ -0,0 +1,3 @@
+package com.example.rest_app2;
+
+public record Greeting(long id, String content) { }
